c48fd58123f1fdbf31776e9a6662f69dfc48d5d1
ConnorMcDonough <ConnorMcDonough@users.noreply.github.com>
2020-10-07 14:40:04 -0400

    Adding Dead Sells Game
    yeeet

---
+++ Assets/QuitButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QuitButtonScript : MonoBehaviour
+{
+    Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            whenClicked();
+
+        });
+    }
+    void whenClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
---
+++ Assets/Standard Assets/2D/Animations/FlutterCOde.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlutterCOde : MonoBehaviour
+
+{
+    public int speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed*Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
