f51e1f96038e47faf7f26bbfab2cfd67838345cf
peterskeion <56321715+peterskeion@users.noreply.github.com>
2020-10-09 22:56:44 -0400

    Added the Damager Script
    This technically gives the ability for anything with this scripts name to die if the animaton exsists for it.
    
    +Skeletons still move while dead because i am unsure what script is making them move
    +Also i have a dying animation need to make a DEAD animation. one that will loop the dead body

---
+++ Assets/DamageScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public int maxHealth;
+    int currentHealth;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("IFTakenHit");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        
+        }
+
+    }
+
+    void Die() 
+    {
+        animator.SetBool("IfDead", true);
+        Debug.Log("Enemy Died!");
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyJumpScript>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+
+        this.enabled = false;
+    
+    }
+
+
+}
---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public Transform attackPoint2;
+
+    public float attackRange= .05f;
+    public float attackRange2 = .05f;
+    public int attackDamage = 20;
+
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        { 
+            Attack(); 
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Attack2();
+        }
+    }
+
+
+
+    void Attack() {
+
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint2.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+           enemy.GetComponent<DamageScript>().TakeDamage(attackDamage);
+            
+
+        }
+
+
+    }
+
+    void Attack2() {
+
+        animator.SetTrigger("Attack2");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+            enemy.GetComponent<DamageScript>().TakeDamage(attackDamage);
+            
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(attackPoint2.position, attackRange2);
+    }
+
+
+
+}
